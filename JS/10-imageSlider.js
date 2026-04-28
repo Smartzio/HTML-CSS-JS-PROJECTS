ffd76b82f19b4ece47316b4f3e55d0e4b02173a8
Mr Solomon Samuel <solowise.420@gmail.com>
2026-04-28 15:45:29 +0100

    About to complete the image slide project

---
+++ JS/10-imageSlider.js
@@ -0,0 +1,35 @@
+// slide data
+const slides = [
+  {
+    Image: "images/10image-Slider/img1.jpg",
+    title: "Power bike",
+    caption: "The Ghost rider's lead"
+  },
+  {
+    Image: "images/10image-Slider/img2.jpg",
+    title: "Cyber Legend",
+    caption: "The master of coding and security"
+  },
+  {
+    Image: "images/10image-Slider/img3.jpg",
+    title: "Captain America",
+    caption: "The legend of the military"
+  },
+  {
+    Image: "images/10image-Slider/img4.jpg",
+    title: "Spider Man",
+    caption: "The super-hero that i love soo much"
+  },
+  {
+    Image: "images/10image-Slider/img5.jpg",
+    title: "The Auto Car",
+    caption: "The ride will make you astonished"
+  },
+];
+
+// Grab elements
+const slider = document.getElementById("slider");
+const sliderTrack = document.getElementById("sliderTrack");
+const dotsContainer = document.getElementById("dots")
+const pervBtn = document.getElementById("pervBtn");
+const nextBtn = document.getElementById("nextBtn");
