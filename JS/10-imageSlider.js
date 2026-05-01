4f2b564dd8b56fe62b03030b7b37cc21f00f40dc
Mr Solomon Samuel <solowise.420@gmail.com>
2026-05-01 14:24:51 +0100

    added clone-practice to .gitingnore

--- JS/10-imageSlider.js
+++ JS/10-imageSlider.js
@@ -1,27 +1,27 @@
 // slide data
 const slides = [
   {
-    Image: "images/10image-Slider/img1.jpg",
+    Image: "images/10Image-Slider/img1.jpg",
     title: "Power bike",
     caption: "The Ghost rider's lead"
   },
   {
-    Image: "images/10image-Slider/img2.jpg",
+    Image: "images/10Image-Slider/img2.jpg",
     title: "Cyber Legend",
     caption: "The master of coding and security"
   },
   {
-    Image: "images/10image-Slider/img3.jpg",
+    Image: "images/10Image-Slider/img3.jpg",
     title: "Captain America",
     caption: "The legend of the military"
   },
   {
-    Image: "images/10image-Slider/img4.jpg",
+    Image: "images/10Image-Slider/img4.jpg",
     title: "Spider Man",
     caption: "The super-hero that i love soo much"
   },
   {
-    Image: "images/10image-Slider/img5.jpg",
+    Image: "images/10Image-Slider/img5.jpg",
     title: "The Auto Car",
     caption: "The ride will make you astonished"
   },
